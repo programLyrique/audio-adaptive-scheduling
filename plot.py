844a4c70a9401aacfdeb62ee09f89b3da9102267
Pierre Donat-Bouillud <pierre.donat-bouillud@ircam.fr>
2016-06-14 15:06:09 +0200

    Correct unit in displaying execution time graph.

--- plot.py
+++ plot.py
@@ -10,7 +10,7 @@ from datetime import timedelta
 
 fileName = sys.argv[1]
 
-data = np.genfromtxt(fileName, delimiter='\t', dtype=float, names=['start', 'end', 'processing'], skip_header=1)
+data = np.genfromtxt(fileName, delimiter='\t', dtype=float, names=['start', 'end', 'processing', 'ratio'], skip_header=1)
 
 callback_durations = data['end'] - data['start']
 
@@ -18,11 +18,11 @@ callback_durations = data['end'] - data['start']
 duration_callback = timedelta(seconds = callback_durations.mean())
 print("Mean duration for callback: ", duration_callback.microseconds, "microseconds")
 
-duration_processing = data['processing'].mean()
-print("Mean duration for processing: ", duration_processing / 1000, "ms")
+duration_processing = data['processing'].mean()#In nanoseconds
+print("Mean duration for processing: ", duration_processing / 1000, "microseconds")
 
 plt.plot(data['start'], callback_durations, label="Duration of callback")
-plt.plot(data['start'], data['processing'], label="Duration of audio processing")
+#plt.plot(data['start'], data['processing'], label="Duration of audio processing")
 
 #plt.axis([0,1,0,2])
 plt.legend(loc='best')
