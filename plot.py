b341c1f48576b2155328e35f7a846aa1f5b0c2ab
Pierre Donat-Bouillud <pierre.donat-bouillud@ircam.fr>
2016-06-21 14:08:03 +0200

    Displaying mean execution times depending on quality for resampler

--- plot.py
+++ plot.py
@@ -6,17 +6,32 @@ import numpy as np
 import sys
 from datetime import timedelta
 
-
+def cluster_per_ratio(data, callback_durations):
+    ratios = {}
+    durations = {}
+    for row, dur in zip(data, callback_durations):
+        ratios.setdefault(row['ratio'], []).append(row)
+        durations.setdefault(row['ratio'], []).append(dur)
+    for key in ratios.keys():
+            ratios[key] = np.array(ratios[key])
+            durations[key] = np.array(durations[key])
+    return (ratios,durations)
 
 fileName = sys.argv[1]
 
 data = np.genfromtxt(fileName, delimiter='\t', dtype=float, names=['start', 'end', 'processing', 'ratio'], skip_header=1)
 
+
 callback_durations = data['end'] - data['start']
 
+clusters,durations = cluster_per_ratio(data, callback_durations)
 
 duration_callback = timedelta(seconds = callback_durations.mean())
 print("Mean duration for callback: ", duration_callback.microseconds, "microseconds")
+
+for key in clusters.keys():
+    print("Mean duration for resampling with ratio ", key, " is: ", clusters[key]['processing'].mean() / 1000)
+
 
 duration_processing = data['processing'].mean()#In nanoseconds
 print("Mean duration for processing: ", duration_processing / 1000, "microseconds")
