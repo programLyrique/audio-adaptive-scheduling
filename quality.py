c9c98c271a5aba03f6eb7621dcf8c4e6cd3df2a9
Pierre Donat-Bouillud <pierre.donat-bouillud@ircam.fr>
2019-02-05 00:27:06 +0100

    Comparing spectograms to ass quality

---
+++ quality.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+"""
+Compares two audio files, one seen as a baseline,the other one degraded,
+using their spectrograms psychoacoustically weighted.
+"""
+import sys
+import librosa
+import numpy as np
+
+
+basefile = sys.argv[1]
+degradedfile = sys.argv[2]
+
+print("Comparing basefile ", basefile, " and degraded file ", degradedfile)
+
+bf,sr1 = librosa.load(basefile, sr=None)
+df,sr2 = librosa.load(basefile, sr=None)
+
+assert(sr1 == sr2)
+
+# See here: http://librosa.github.io/librosa/generated/librosa.core.perceptual_weighting.html
+def perceptual_cqt(y,sr):
+    C = np.abs(librosa.cqt(y, sr=sr, fmin=librosa.note_to_hz('A1')))
+    freqs = librosa.cqt_frequencies(C.shape[0], fmin=librosa.note_to_hz('A1'))#Adapted to music
+    perceptual_CQT = librosa.perceptual_weighting(C**2, freqs, ref=np.max)# Uses A-weighting
+    return perceptual_CQT
+
+base_pcat = perceptual_cqt(bf, sr1)
+degraded_pcat = perceptual_cqt(df, sr2)
+
+quality = np.linalg.norm(base_pcat - degraded_pcat)
+
+print("Quality is ", quality)
