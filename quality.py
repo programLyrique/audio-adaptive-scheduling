a17886a51430874da18e4e597754b111f1a4eae9
Pierre Donat-Bouillud <pierre.donat-bouillud@ircam.fr>
2019-02-05 15:48:37 +0100

    Quality between 0 and 1.

--- quality.py
+++ quality.py
@@ -23,12 +23,14 @@ assert(sr1 == sr2)
 def perceptual_cqt(y,sr):
     C = np.abs(librosa.cqt(y, sr=sr, fmin=librosa.note_to_hz('A1')))
     freqs = librosa.cqt_frequencies(C.shape[0], fmin=librosa.note_to_hz('A1'))#Adapted to music
-    perceptual_CQT = librosa.perceptual_weighting(C**2, freqs, ref=np.max)# Uses A-weighting
+    perceptual_CQT = librosa.perceptual_weighting(C**2, freqs, ref=np.max)# Uses
+    # https://en.wikipedia.org/wiki/ITU-R_468_noise_weighting This one seems to be better for high freq
+    # because A-weighting does not cut enough high frequencies
     return perceptual_CQT
 
 base_pcat = perceptual_cqt(bf, sr1)
 degraded_pcat = perceptual_cqt(df, sr2)
 
-quality = np.linalg.norm(base_pcat - degraded_pcat)
+quality = np.exp(- np.linalg.norm(base_pcat - degraded_pcat))
 
 print("Quality is ", quality)
